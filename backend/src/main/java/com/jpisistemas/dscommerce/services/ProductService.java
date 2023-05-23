2f6f0e1ed68fd6b2d1be5d616156f1423eb86131
jeanfurquim <jean.furquim@jpisistemas.com.br>
2023-05-23 00:20:34 -0300

    Porduct per page

--- backend/src/main/java/com/jpisistemas/dscommerce/services/ProductService.java
+++ backend/src/main/java/com/jpisistemas/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.jpisistemas.dscommerce.dto.ProductDTO;
 import com.jpisistemas.dscommerce.entities.Product;
 import com.jpisistemas.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,8 +19,15 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-       Product product = repository.findById(id).get();
-       return  new ProductDTO(product);
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
 
     }
 }
