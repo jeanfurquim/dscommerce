aa174d78425d4a2b2b22d7464d05412d95a90d66
jeanfurquim <jean.furquim@jpisistemas.com.br>
2023-05-24 01:24:25 -0300

    Bean validation

--- backend/src/main/java/com/jpisistemas/dscommerce/dto/ProductDTO.java
+++ backend/src/main/java/com/jpisistemas/dscommerce/dto/ProductDTO.java
@@ -1,18 +1,22 @@
 package com.jpisistemas.dscommerce.dto;
 
 import com.jpisistemas.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
 
+    @Size(min = 10, message = "Nome precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
-    public ProductDTO() {
-
-    }
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
